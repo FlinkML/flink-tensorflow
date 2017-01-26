b154891214123be6a21e79d2932635c3e70a0380
EronWright <ewright@live.com>
2017-01-26 14:36:59 -0800

    WholeFileInputFormat

---
+++ src/main/scala/org/apache/flink/contrib/tensorflow/examples/inception/ImageInputFormat.scala
@@ -0,0 +1,54 @@
+package org.apache.flink.contrib.tensorflow.examples.inception
+
+import java.io.IOException
+import java.util.Collections
+
+import org.apache.flink.contrib.tensorflow.io.WholeFileInputFormat
+import org.apache.flink.core.fs.{FSDataInputStream, Path}
+import ImageNormalization.Image
+import WholeFileInputFormat._
+import org.apache.flink.api.common.io.GlobFilePathFilter
+import org.apache.flink.configuration.Configuration
+
+import scala.collection.JavaConverters._
+
+/**
+  * Input format for images.
+  */
+class ImageInputFormat extends WholeFileInputFormat[(String,Image)] {
+
+  def this(includePattern: String) {
+    this()
+    setFilesFilter(new GlobFilePathFilter(List(includePattern).asJava, Collections.emptyList()))
+  }
+
+  /**
+    * This function parses the given file stream which represents a serialized record.
+    * The function returns a valid record or throws an IOException.
+    *
+    * @param reuse      An optionally reusable object.
+    * @param fileStream The file input stream.
+    * @return Returns the read record if it was successfully read.
+    * @throws IOException if the record could not be read.
+    */
+  override def readRecord(reuse: (String,Image), filePath: Path, fileStream: FSDataInputStream, fileLength: Long): (String,Image) = {
+    if(fileLength > Int.MaxValue) {
+      throw new IllegalArgumentException("the file is too large to be fully read")
+    }
+    val name = filePath.getName
+    reuse match {
+      case (_: String, r: Array[Byte]) if r.length == fileLength => (name, readFully(fileStream, r, 0, r.length))
+      case _ => (name, readFully(fileStream, new Array[Byte](fileLength.toInt), 0, fileLength.toInt))
+    }
+  }
+
+//  override def readRecord(reuse: Image, filePath: Path, fileStream: FSDataInputStream, fileLength: Long): Image = {
+//    if(fileLength > Int.MaxValue) {
+//      throw new IllegalArgumentException("the file is too large to be fully read")
+//    }
+//    reuse match {
+//      case r: Array[Byte] if r.length == fileLength => readFully(fileStream, r, 0, r.length)
+//      case _ => readFully(fileStream, new Array[Byte](fileLength.toInt), 0, fileLength.toInt)
+//    }
+//  }
+}
--- src/main/scala/org/apache/flink/contrib/tensorflow/examples/inception/app.scala
+++ src/main/scala/org/apache/flink/contrib/tensorflow/examples/inception/app.scala
@@ -7,6 +7,8 @@ import org.apache.flink.contrib.tensorflow.examples.inception.InceptionModel.Lab
 import org.apache.flink.contrib.tensorflow.streaming._
 import org.apache.flink.contrib.tensorflow.types.Rank.`4D`
 import org.apache.flink.contrib.tensorflow.types.TensorValue
+import org.apache.flink.streaming.api.functions.source.FileProcessingMode.PROCESS_CONTINUOUSLY
+import scala.concurrent.duration._
 
 /**
   * A streaming image labeler, based on the 'inception5h' model.
@@ -20,23 +22,25 @@ object Inception {
     env.setParallelism(1)
 
     if (args.length < 2) {
-      System.out.println("Usage: Inception <model-dir> <image-path...>")
+      System.out.println("Usage: Inception <model-dir> <images-dir>")
       System.exit(1)
     }
-    val modelPath = Paths.get(args.toSeq.head).toUri
-    val images = args.toSeq.tail.map(readImage)
+    val modelPath = Paths.get(args(0)).toUri
+    val imagesPath = args(1)
 
+    val fileStream = env.readFile(new ImageInputFormat, imagesPath, PROCESS_CONTINUOUSLY, (1 second).toMillis)
 
     // normalize the raw image as a 4D image tensor
     val normalizationModel = new ImageNormalization()
-    val imageStream: DataStream[(String, TensorValue[`4D`,Float])] = env
-      .fromCollection(images)
-      .mapWithModel(normalizationModel) { (in, model) =>
+
+    val imageStream: DataStream[(String, TensorValue[`4D`,Float])] =
+      fileStream.mapWithModel(normalizationModel) { (in, model) =>
         (in._1, model.run(Seq(in._2))(model.normalize))
       }
 
     // label the image tensor using the inception5h model
     val inceptionModel = new InceptionModel(modelPath)
+
     val labelStream: DataStream[(String,LabeledImage)] = imageStream
       .mapWithModel(inceptionModel) { (in, model) =>
         val labelTensor = model.run(in._2)(model.label)
@@ -47,11 +51,6 @@ object Inception {
 
     // execute program
     env.execute("Inception")
-  }
-
-  def readImage(localPath: String): (String,Image) = {
-    val path = FileSystems.getDefault.getPath(localPath)
-   (path.getFileName.toString, Files.readAllBytes(path))
   }
 }
 
