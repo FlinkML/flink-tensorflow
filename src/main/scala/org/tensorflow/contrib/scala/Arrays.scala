6b439fc073f0b633027cccaeb70c3aec271a84e0
EronWright <ewright@live.com>
2017-03-24 00:09:17 -0700

    conversions

--- src/main/scala/org/tensorflow/contrib/scala/Arrays.scala
+++ src/main/scala/org/tensorflow/contrib/scala/Arrays.scala
@@ -3,9 +3,8 @@ package org.tensorflow.contrib.scala
 import java.nio.{DoubleBuffer, FloatBuffer, IntBuffer, LongBuffer}
 
 import com.twitter.bijection.Bijection
-import org.apache.flink.api.java.tuple.Tuple
 import org.tensorflow.Tensor
-import org.tensorflow.contrib.scala.Rank.`1D`
+import org.tensorflow.contrib.scala.Rank._
 
 /**
   * Support for Scala arrays.
@@ -15,7 +14,7 @@ object Arrays {
   /**
     * Convert a [[Array]] of [[Int]]s to a 1-D [[Tensor]].
     */
-  implicit def intArray2Tensor: Bijection[Array[Int],TypedTensor[`1D`,Int]] =
+  implicit val intArray2Tensor: Bijection[Array[Int],TypedTensor[`1D`,Int]] =
     Bijection.build[Array[Int], TypedTensor[`1D`,Int]] { arry =>
       Tensor.create(arry).taggedWith[`1D`,Int]
     } { t =>
@@ -27,7 +26,7 @@ object Arrays {
   /**
     * Convert a [[Array]] of [[Long]]s to a 1-D [[Tensor]].
     */
-  implicit def longArray2Tensor[R <: Tuple]: Bijection[Array[Long],TypedTensor[`1D`,Long]] =
+  implicit val longArray2Tensor: Bijection[Array[Long],TypedTensor[`1D`,Long]] =
     Bijection.build[Array[Long], TypedTensor[`1D`,Long]] { arry =>
       Tensor.create(arry).taggedWith[`1D`,Long]
     } { t =>
@@ -39,7 +38,7 @@ object Arrays {
   /**
     * Convert a [[Array]] of [[Float]]s to a 1-D [[Tensor]].
     */
-  implicit def floatArray2Tensor[R <: Tuple]: Bijection[Array[Float],TypedTensor[`1D`,Float]] =
+  implicit val floatArray2Tensor: Bijection[Array[Float],TypedTensor[`1D`,Float]] =
     Bijection.build[Array[Float], TypedTensor[`1D`,Float]] { arry =>
       Tensor.create(arry).taggedWith[`1D`,Float]
     } { t =>
@@ -51,7 +50,7 @@ object Arrays {
   /**
     * Convert a [[Array]] of [[Double]]s to a 1-D [[Tensor]].
     */
-  implicit def doubleArray2Tensor[R <: Tuple]: Bijection[Array[Double],TypedTensor[`1D`,Double]] =
+  implicit val doubleArray2Tensor: Bijection[Array[Double],TypedTensor[`1D`,Double]] =
     Bijection.build[Array[Double], TypedTensor[`1D`,Double]] { arry =>
       Tensor.create(arry).taggedWith[`1D`,Double]
     } { t =>
