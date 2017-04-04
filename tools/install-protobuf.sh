06264de343d8d68adcf38523a6c1195f8e241f90
EronWright <ewright@live.com>
2017-04-04 07:58:11 -0700

    travis

--- tools/install-protobuf.sh
+++ tools/install-protobuf.sh
@@ -2,9 +2,9 @@
 set -e
 # check to see if protobuf folder is empty
 if [ ! -d "$HOME/protobuf/lib" ]; then
-  wget https://github.com/google/protobuf/releases/download/v3.0.0/protobuf-java-3.0.0.zip
-  unzip -o protobuf-java-3.0.0
-  cd protobuf-java-3.0.0 && ./configure --prefix=$HOME/protobuf && make && make install
+  wget -q https://github.com/google/protobuf/releases/download/v3.0.0/protobuf-java-3.0.0.zip
+  unzip -q -o protobuf-java-3.0.0
+  cd protobuf-3.0.0 && ./configure --prefix=$HOME/protobuf && make && make install
 else
   echo "Using cached directory."
 fi
